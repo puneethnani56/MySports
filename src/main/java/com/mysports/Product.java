4cdb09625d3d2d2ea024dfe7a372df7f0ed3d407
puneethnani <puneethnani56@gmail.com>
2025-08-03 21:28:11 +0530

    Update Product.java

--- src/main/java/com/mysports/Product.java
+++ src/main/java/com/mysports/Product.java
@@ -1,10 +1,14 @@
+package com.mysports;
+
 public class Product {
     private String id;
     private String name;
     private double price;
 
     public Product(String id, String name, double price) {
-        this.id = id; this.name = name; this.price = price;
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 
     public String getId() { return id; }
