a13089d10fa6561ec5ee28703c8e47c0b6a28cf3
puneethnani <puneethnani56@gmail.com>
2025-08-03 21:37:34 +0530

    Update ProductServlet.java

--- src/main/java/com/mysports/ProductServlet.java
+++ src/main/java/com/mysports/ProductServlet.java
@@ -9,10 +9,16 @@ public class ProductServlet extends HttpServlet {
 
     public void init() throws ServletException {
         List<Product> productList = new ArrayList<>();
-
+        
         productList.add(new Product("S001", "Football", 500));
         productList.add(new Product("S002", "Tennis Racket", 1200));
         productList.add(new Product("S003", "Cricket Bat", 2500));
+        productList.add(new Product("S004", "Badminton Shuttle", 200));
+        productList.add(new Product("S005", "Hockey Stick", 1500));
+        productList.add(new Product("S006", "Basketball", 700));
+        productList.add(new Product("S007", "Table Tennis Bat", 900));
+        productList.add(new Product("S008", "Boxing Gloves", 1300));
+
 
         getServletContext().setAttribute("productList", productList);
     }
