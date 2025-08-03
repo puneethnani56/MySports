ad3a94a0eccb4eede8fe58852ad41419561094df
puneethnani <puneethnani56@gmail.com>
2025-08-03 21:24:03 +0530

    Create ProductServlet.java

---
+++ src/main/java/com/mysports/ProductServlet.java
@@ -0,0 +1,24 @@
+package com.mysports;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.*;
+
+public class ProductServlet extends HttpServlet {
+
+    public void init() throws ServletException {
+        List<Product> productList = new ArrayList<>();
+
+        productList.add(new Product("S001", "Football", 500));
+        productList.add(new Product("S002", "Tennis Racket", 1200));
+        productList.add(new Product("S003", "Cricket Bat", 2500));
+
+        getServletContext().setAttribute("productList", productList);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+    }
+}
